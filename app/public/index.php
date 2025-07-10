a9a42246b454136609eafd04acebf0ecb8a122fb
j4jet <jrex4jet@gmail.com>
2025-07-09 21:57:14 -0400

    Set up basic files and folders to have a static webpage with some text on it:
    - app/php/index.php is just the main php file for now
    - nginx/conf.d/default.conf is just the nginx conf file
    
    I've also set up a Docker compose file and Dockerfile for php.

---
+++ app/public/index.php
@@ -0,0 +1,5 @@
+<?php
+
+echo"<h1>Welcome to My Personal Page</h1>";
+echo "<h2>Allosaurus is a genus of theropod dinosaur from the Late Jurassic period, known for its large size and distinctive features. It was a bipedal predator with sharp teeth and claws, adapted for hunting and scavenging. Fossils of Allosaurus have been found in North America, providing valuable insights into the diversity of Jurassic ecosystems.</h2>";
+echo "<i>I like dinosaurs, especially Allosaurus!</i>";
