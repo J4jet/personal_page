e9cdca856cb585a89e38fb7486c72139824b4eaf
j4jet <jrex4jet@gmail.com>
2025-08-17 13:39:17 -0400

    Set up basic contact page - added contact.php - updaded style.css accordingly

---
+++ app/public/contact.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Personal Page</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <a href="index.php" class="header-name">Justin Forgette</a>
+        <nav>
+            <ul>
+                <li><a href="about.php">About Me</a></li>
+                <li><a href="projects.php">Projects</a></li>
+                <li><a href="contact.php">Contact</a></li>
+            </ul>
+            <a href="https://github.com/J4jet" target="_blank" rel="noopener noreferrer" class="header-github">GitHub</a>
+        </nav>
+    </header>
+    <main>
+        <div class="wrapper">
+            <section class="contact-intro">
+                <h1>Contact</h1>
+            </section>
+            <section class="contact-list">
+                <article class="contact-item">
+                    <h2>Email: justin.forgette119@gmail.com</h2>
+                    <h2>LinkedIn: <a href="https://www.linkedin.com/in/justin-forgette/" target="_blank" rel="noopener noreferrer" class="contact-linked">Justin Forgette</a></h2>
+                    <h2>GitHub: <a href="https://github.com/J4jet" target="_blank" rel="noopener noreferrer" class="contact-linked">J4jet</a></h2>
+                </article>
+            </section>
+        </div>
+    </main>
+    <div class="wrapper">
+        <footer>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="about.php">About Me</a></li>
+                <li><a href="projects.php">Projects</a></li>
+                <li><a href="contact.php">Contact</a></li>
+            </ul>
+            <div class="footer-social">
+                <a href="https://github.com/J4jet" target="_blank" rel="noopener noreferrer"><img src="img/github.png" alt="GitHub"></a>
+                <a href="https://www.linkedin.com/in/justin-forgette/" target="_blank" rel="noopener noreferrer"><img src="img/linked.png" alt="LinkedIn"></a>
+                <a href="https://www.instagram.com/j4jet26/" target="_blank" rel="noopener noreferrer"><img src="img/insta.png" alt="Instagram"></a>
+            </div>
+        </footer>
+    </div>
+</body>
+</html>
